1b608b3829b493d552ea09b58ae2eccee21abbc6
Moklesur Rahman <80785811+Moklesur-Rahman2@users.noreply.github.com>
2022-05-19 04:24:55 +0600

    update url

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -12,7 +12,7 @@ const Welcome = () => {
 
             <div className="login mt-8">
                 <Link to='/login' className="btn btn-active btn-primary">Login</Link>
-                <Link to='signup' className="btn btn-active btn-secondary ml-5">Sign Up</Link>
+                <Link to='/signup' className="btn btn-active btn-secondary ml-5">Sign Up</Link>
             </div>
         </section>
     );
